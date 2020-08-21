3cd8955ffda61c517cce74ba818e7ce955f7d5db
Arpit <arpit.choudhary@timesinternet.in>
2020-08-21 14:57:54 +0530

    update

--- app/src/main/java/com/example/navigationcomponent/ConfirmationFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/ConfirmationFragment.kt
@@ -5,8 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.navigationcomponent.model.Money
+import kotlinx.android.synthetic.main.fragment_confirmation.*
 
 class ConfirmationFragment : Fragment() {
+
+    private lateinit var recipient: String
+    private lateinit var mon: Money
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = requireArguments().getString("recipient").toString()
+        mon = requireArguments().getParcelable("amount")!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +25,13 @@ class ConfirmationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val result = "You have sent $${mon.amount} to $recipient"
+        confirmation_message.text = result
     }
 
 }
--- app/src/main/java/com/example/navigationcomponent/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/SpecifyAmountFragment.kt
@@ -1,17 +1,29 @@
 package com.example.navigationcomponent
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.example.navigationcomponent.model.Money
+import com.example.navigationcomponent.util.showToast
 import kotlinx.android.synthetic.main.fragment_specify_amount.*;
+import kotlinx.android.synthetic.main.fragment_specify_amount.cancel_btn
+import java.math.MathContext
 
 class SpecifyAmountFragment : Fragment(), View.OnClickListener {
 
     private lateinit var navController: NavController
+    private lateinit var recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = requireArguments().getString("recipient").toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +42,17 @@ class SpecifyAmountFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v!!.id) {
+
             R.id.send_btn -> {
-                navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+                if (!TextUtils.isEmpty(input_amount.text.toString())) {
+                    val bundle = bundleOf(
+                        "recipient" to recipient,
+                         "amount" to Money(input_amount.text.toString().toBigDecimal(MathContext.DECIMAL128))
+                    )
+                    navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment, bundle)
+                } else {
+                    activity?.showToast("Enter Recipient!!!")
+                }
             }
 
             R.id.cancel_btn -> {
